0cbce032288bccb8cd7f4174c9732a564552ad25
peter <pwesterbaan2@gmail.com>
2018-05-29 15:39:17 -0400

    Trying to be slick in saving my ip address in case it changes

--- .sleepWorkhorse.sh
+++ .sleepWorkhorse.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+ifconfig | grep -Po "inet addr:.+Bcast" | grep -Po "(?:\d{1,3}\.){3}\d{1,3}" > ~/Dropbox/Documents/workhorseIP.txt && cat ~/Dropbox/Documents/workhorseIP.txt
 echo 'pm-suspend' | sudo at now + 1 minutes
