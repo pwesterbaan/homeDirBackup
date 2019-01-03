825ef56cd1003bbfbd056e48c12e975910e86597
pwesterbaan <pwesterbaan2@gmail.com>
2019-01-03 14:06:02 -0500

    Updated MATLAB alias

--- .resetMonitors.sh
+++ .resetMonitors.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 ### Copy backup of displays file
-cp .config/xfce4/xfconf/xfce-perchannel-xml/displays.xml.bak .config/xfce4/xfconf/xfce-perchannel-xml/displays.xml
+###cp .config/xfce4/xfconf/xfce-perchannel-xml/displays.xml.bak .config/xfce4/xfconf/xfce-perchannel-xml/displays.xml
 
+### xrandr to find info
+xrandr --output DVI-0 --mode 1280x1024 --right-of DVI-1-0
