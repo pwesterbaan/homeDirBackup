dbcdd3f6fb68be545b5e40bef863e13469e5689e
pwesterbaan <pwesterbaan2@gmail.com>
2019-08-05 12:28:06 -0400

    Added temperature to Conky

--- .getWeather.sh
+++ .getWeather.sh
@@ -2,8 +2,12 @@
 
 ## Simple script to return basic weather info
 ## Change url below to match desired city
-URL='https://www.accuweather.com/en/us/clemson-sc/29631/weather-forecast/335501'
+#URL='https://www.accuweather.com/en/us/clemson-sc/29631/weather-forecast/335501'
 
 #wget -q -O- "$URL" | awk -F\" '/acm_RecentLocationsCarousel\.push/{print $2"\n"$4"\n"}' | head -2
-wget -qO- "$URL" | awk -F\' '/acm_RecentLocationsCarousel\.push/{print "  "$10"°"}' | head -1
-wget -qO- "$URL" | awk -F\" '/acm_RecentLocationsCarousel\.push/{print "  "$4"\n"}' | head -1
+#wget -qO- "$URL" | awk -F\' '/acm_RecentLocationsCarousel\.push/{print "  "$10"°"}' | head -1
+#wget -qO- "$URL" | awk -F\" '/acm_RecentLocationsCarousel\.push/{print "  "$4"\n"}' | head -1
+
+## weather Clemson, SC and then get fips code from results
+weather fips4514950 --headers=Temperature | grep Temperature | cut -c 17-22
+
