86cd8e796997af533fb08e2e98c7f485832b11de
pwesterbaan <pwesterbaan2@gmail.com>
2020-07-09 12:13:25 -0400

    Updated comment about linking script to rc.local

--- .emailIp.sh
+++ .emailIp.sh
@@ -2,5 +2,5 @@
 
 ## send email containing IP address
 ## https://media.yuis-programming.com/how-to-run-scripts-when-rebooted-startup-on-ubuntu-18-04/
-## placed in /etc/rc.local
+## link to this script placed in /etc/rc.local
 ifconfig | grep "inet" | grep -Po "(?:\d{1,3}\.){3}\d{1,3}" | head -n 1 | /usr/sbin/ssmtp pwesterbaan2@gmail.com
