116bc45cf98962db05a73521217572d4d3a709a6
pwesterbaan <pwesterbaan2@gmail.com>
2020-07-18 14:46:27 -0400

    Created spamming script because why not

---
+++ .scripts/emailSpam.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#call this script: emailSpam.sh <recipient> <number of iterations> <message to repeat>
+
+#2nd and 3rd argument are optional
+numIters=${2:-50}
+message=${3:-"boop "}
+
+echo "Iterations:"$numIters
+echo "Message:   "$message
+touch $1_tmp.txt
+for i in $(seq 1 $numIters);
+  do
+  echo $i
+  echo -n ""> $1_tmp.txt;
+  for j in $(seq 1 $i);
+    do
+      echo -n "$message" >> $1_tmp.txt;
+    done;
+  echo "" >> $1_tmp.txt;
+  sleep $i;
+  ssmtp $1 < $1_tmp.txt;
+done
+rm $1_tmp.txt
