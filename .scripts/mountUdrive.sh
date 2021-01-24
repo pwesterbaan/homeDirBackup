5806fdc5a186b06d0c3e15e26d42bada9092145e
pwesterbaan <pwesterbaan2@gmail.com>
2021-01-24 10:53:41 -0500

    Added full home path for use with sudo

--- .scripts/mountUdrive.sh
+++ .scripts/mountUdrive.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-if [ ! -d ~/Desktop/uDrive ]; then
-    mkdir ~/Desktop/uDrive/
+if [ ! -d /home/peter/Desktop/uDrive ]; then
+    mkdir /home/peter/Desktop/uDrive/
 fi
 sudo mount -t cifs -o credentials=/root/smbpass.txt //home.clemson.edu/pwester /home/peter/Desktop/uDrive
