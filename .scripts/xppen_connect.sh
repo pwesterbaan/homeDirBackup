00e82500ed4f94d7c1e069981e5ac621fcb0974f
Peter Westerbaan <pwesterbaan2@gmail.com>
2025-01-27 07:55:33 -0500

    Modified for new office setup

--- .scripts/xppen_connect.sh
+++ .scripts/xppen_connect.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-SCREENNAME=$(xrandr | awk '/eDP/ { print $1}')
+# SCREENNAME=$(xrandr | awk '/eDP/ { print $1}')
+SCREENNAME=$(xrandr | awk '/HDMI/ { print $1}')
 DEVID=$(xinput | awk '/UGTABLET 6 inch PenTablet Pen/ { print $9}' | cut -d '=' -f 2)
 
 xinput map-to-output $DEVID $SCREENNAME
