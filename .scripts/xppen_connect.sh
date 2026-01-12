85123d4bb5a1169bd79bec66b9eb7eb4fe54171e
Peter Westerbaan <pwesterbaan2@gmail.com>
2026-01-12 10:38:05 -0500

    Switched to lowercase variable names

--- .scripts/xppen_connect.sh
+++ .scripts/xppen_connect.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# SCREENNAME=$(xrandr | awk '/eDP/ { print $1}')
-SCREENNAME=$(xrandr | awk '/HDMI/ { print $1}')
-DEVID=$(xinput | awk '/UGTABLET 6 inch PenTablet Pen/ { print $9}' | cut -d '=' -f 2)
+# screenname=$(xrandr | awk '/eDP/ { print $1}')
+screenname=$(xrandr | awk '/HDMI/ { print $1}')
+devid=$(xinput | awk '/UGTABLET 6 inch PenTablet Pen/ { print $9}' | cut -d '=' -f 2)
 
-xinput map-to-output $DEVID $SCREENNAME
+xinput map-to-output $devid $screenname
