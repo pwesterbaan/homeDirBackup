62069cb35469cd4e8f7ba554cdd989bd226f9f12
Peter Westerbaan <pwesterbaan2@gmail.com>
2026-03-17 17:19:18 -0400

    Opens notification settings after restarting notifyd

--- .scripts/fix_notifyd.sh
+++ .scripts/fix_notifyd.sh
@@ -2,11 +2,14 @@
 
 #Some combination of these commands seemed to fix my broken notifications
 pkill dunst
+sleep 1
 pkill xfce4-notifyd
 
 #check installation
 # dpkg -l | grep xfce4-notifyd
 #check startup
 # journalctl -xe | grep notifyd
-sleep 2
+
+xfce4-notifyd-config
+# sleep 2
 notify-send  "Notifications fixed!"
