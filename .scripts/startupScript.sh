fc88b0f604215e246bc0928ccfda34775717a6af
Peter Westerbaan <pwesterbaan2@gmail.com>
2026-04-09 08:51:27 -0400

    Added fix_notifyd to startup script

--- .scripts/startupScript.sh
+++ .scripts/startupScript.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 sleep 10
-/home/peter/.scripts/resetMonitors.sh
+home_dir="/home/peter"
+cd "$home_dir"
+.scripts/resetMonitors.sh
 
 # /snap/bin/chromium --profile-directory=Default --app-id=faolnafnngnfdaknnbpnkhgohbobgegn &
 # /snap/bin/chromium --profile-directory=Default --app-id=hpfldicfbfomlpcikngkocigghgafkph &
@@ -11,5 +13,6 @@ spotify &
 # /home/peter/.scripts/xppen_connect.sh
 
 sleep 5
-/home/peter/.scripts/getWeather.sh &
-/home/peter/.scripts/start-cnky.sh
+.scripts/getWeather.sh &
+.scripts/start-cnky.sh
+.scripts/fix_notifyd.sh
